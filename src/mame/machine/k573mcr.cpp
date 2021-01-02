18cff545d2a4df28cbfbb98daa7f3cfd2a3db935
987123879113 <63495610+987123879113@users.noreply.github.com>
2021-01-02 10:16:16 +0900

    k573mcr: Add PCB documentation

--- src/mame/machine/k573mcr.cpp
+++ src/mame/machine/k573mcr.cpp
@@ -2,15 +2,60 @@
 // copyright-holders:smf
 /*
  * Konami 573 Memory Card Reader
- *
+
+  Main PCB Layout
+  ---------------
+  GE885-PWB(A)A
+  (C)1999 KONAMI CO. LTD.
+  |---------------------------------------------------------|
+  |     PQ30RV11                                            |
+  |                                  |----------|           |
+  |  DIP8          |--------------|  |          |      CN61 |
+  |                |              |  | XCS05/10 |           |
+  |                |              |  |          |           |
+  |                |  TMPR3904AF  |  |----------|           |
+  |                |              |                    CN62 |
+  |                |              |                         |
+  |  CN65          |--------------|                         |
+  |            8.25 MHz                                CN67 |
+  |                                                         |
+  |                                                         |
+  |                                     EP4M16              |
+  |                                                         |
+  |                                                         |
+  |                EDO-DRAM4M-35                            |
+  |                                                         |
+  |                                                         |
+  |                                                 --------|
+  |                                                 |
+  |                                                 |
+  | USB-A                                      CN64 |
+  |                                                 |
+  |          ADM485JR                               |
+  |                                                 |
+  | USB-B                                      CN63 |
+  |                                                 |
+  |-------------------------------------------------|
+
+Notes:
+	DIP8           - 8-position DIP switch
+	CN61           - BS8PSHF1AA 8 pin connector, connects MCR to memory card harness
+	CN62           - BS8PSHF1AA 8 pin connector
+	CN63           - 6P-SHVQ labeled "0", GE885-JB security dongle is connected here
+	CN64           - 6P-SHVQ labeled "1"
+	CN65           - B4PS-VH, 4 pin power connector
+	CN67           - BS15PSHF1AA, 15-pin connector, unpopulated
+	USB-A          - USB-A connector labeled 20J
+	USB-B          - USB-B connector labeled 20L, connects MCR board to USB on System 573 motherboard
+	ADM485JR       - Analog Devices ADM485 low power EIA RS-485 transceiver
+	TMPR3904AF     - Toshiba TMPR3904AF RISC Microprocessor
+	XCS05/10       - XILINX XCS10XL VQ100AKP9909 A2026631A
+	EDO-DRAM4M-35  - Silicon Magic 66 MHz C9742 SM81C256K16CJ-35, 256K x 16 EDO DRAM
+	EP4M16         - ROM labeled "855-A01"
  */
 
 #include "emu.h"
 #include "k573mcr.h"
-
-/*
-  GE885-PWB(A)A ( contains Toshiba tmpr3904af, ram, rom, tranceiver and glue ).
-*/
 
 k573mcr_device::k573mcr_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock) :
 	device_t(mconfig, KONAMI_573_MEMORY_CARD_READER, tag, owner, clock)
