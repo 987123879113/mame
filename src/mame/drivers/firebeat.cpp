68431311565f7685f3625cc4c08f7891a3ee14ae
987123879113 <63495610+987123879113@users.noreply.github.com>
2021-01-21 21:28:27 +0900

    Set default visable area

--- src/mame/drivers/firebeat.cpp
+++ src/mame/drivers/firebeat.cpp
@@ -1220,6 +1220,7 @@ void firebeat_state::firebeat(machine_config &config)
 	screen.set_refresh_hz(60);
 	screen.set_vblank_time(ATTOSECONDS_IN_USEC(2500)); /* not accurate */
 	screen.set_size(512, 384);
+	screen.set_visarea(0, 511, 0, 383);
 	screen.set_screen_update(FUNC(firebeat_state::screen_update_firebeat_0));
 	screen.set_palette("palette");
 
@@ -1273,6 +1274,7 @@ void firebeat_state::firebeat2(machine_config &config)
 	lscreen.set_refresh_hz(60);
 	lscreen.set_vblank_time(ATTOSECONDS_IN_USEC(2500)); /* not accurate */
 	lscreen.set_size(512, 384);
+	lscreen.set_visarea(0, 511, 0, 383);
 	lscreen.set_screen_update(FUNC(firebeat_state::screen_update_firebeat_0));
 	lscreen.set_palette("palette");
 
@@ -1280,6 +1282,7 @@ void firebeat_state::firebeat2(machine_config &config)
 	rscreen.set_refresh_hz(60);
 	rscreen.set_vblank_time(ATTOSECONDS_IN_USEC(2500)); /* not accurate */
 	rscreen.set_size(512, 384);
+	rscreen.set_visarea(0, 511, 0, 383);
 	rscreen.set_screen_update(FUNC(firebeat_state::screen_update_firebeat_1));
 	rscreen.set_palette("palette");
 
