413b7799781e55065286b39bde311eebbc458049
987123879113 <63495610+987123879113@users.noreply.github.com>
2021-01-26 20:40:19 +0900

    Update documentation a little bit

--- src/mame/drivers/firebeat.cpp
+++ src/mame/drivers/firebeat.cpp
@@ -829,35 +829,38 @@ void firebeat_state::lamp_output3_ppp_w(offs_t offset, uint32_t data, uint32_t m
     IRQ2: Executes one command stored in a different buffer from IRQ1.
           The buffer can contain up to 8 commands.
 	      The command index counter increments after each IRQ2 call.
-		  If there is no command in the slot at the current counter
-		  then it just increments without executing a command.
+		  If there is no command in the slot at the current counter then it just increments without executing a command.
+
+		  Timing matters. In particular if the speed of the IRQ 2 calls is too fast then the volume and frequency animations will be wrong.
+		  The most common issue with bad timing is keysounds will be cut off.
+		  pop'n music Animelo 2 also has an issue when playing CHA-LA HEAD CHA-LA where one of the beginning keysounds will stay on a
+		  very high pitched frequency. The lower(?) the IRQ 2 frequency, the longer the keysound stays played it seems.
 
 		  For beatmania III:
 			cmd[0] = nop
 			cmd[1] = 0x91bc -> Send stop command for all rf5c400 channels that are done playing
 			cmd[2] = 0x310a -> Error checking? Sending some kind of state to main CPU???
 			cmd[3] = 0x29c6 -> Increment a timer for each running DMA(ATA command?)
-				Each timer must count up to 0x02e8 (744) before it will move on to the next DMA
+				Each timer must count up to 0x02e8 (744) before it will move on to the next DMA, which I believe is the time out counter.
 
-				In another part of the program (0x363c for a21jca03.bin) is the following code for
-				determining when to start and stop the DMA:
+				In another part of the program (0x363c for a21jca03.bin) is the following code for determining when to start and stop the DMA:
 
 				start_dma();
 				while (get_dma_timer() < dma_max_timer) {
-					if (irq6_was_called_flag) {
+					if (irq6_called_flag) {
 						break;
 					}
 				}
 				end_dma();
-				return;
 
-				irq6_was_called_flag is set only when IRQ6 was called.
-				get_dma_timer is what is incremented by 0x29c6.
-			cmd[4] = 0x94de -> Animates channel volume if required
-			cmd[5] = 0x7b2c -> Send some kind of buffer statuses to spu_status_led_w. Related to IRQ4 since commands come from PPC to set buffer data
-			cmd[6] = 0x977e -> Animates channel frequency if required
+				irq6_called_flag is set only when IRQ6 is called.
+				get_dma_timer is the timer that is incremented by 0x29c6.
+			cmd[4] = 0x94de -> Animates rf5c400 channel volumes
+			cmd[5] = 0x7b2c -> Send some kind of buffer status flags to spu_status_led_w. Related to IRQ4 since commands come from PPC to set buffer data
+			cmd[6] = 0x977e -> Animates rf5c400 channel frequencies
 			cmd[7] = 0x9204 -> Sends current state of rf5c400 channels as well as a list (bitmask integer) of usable channels up to main CPU memory.
 			                   Also sends a flag to to spu_status_led_w that shows if there are available SE slots.
+							   If there are no available SE slots then it will set bit 3 to .
 
     IRQ4: Dual-port RAM mailbox (when PPC writes to 0x3FE)
           Handles commands from PPC (bytes 0x00 and 0x01)
@@ -892,7 +895,9 @@ void firebeat_state::spu_irq_ack_w(offs_t offset, uint16_t data, uint16_t mem_ma
 
 void firebeat_state::spu_status_led_w(uint16_t data)
 {
-	// Set when clearing waveram memory during initialization
+	// Verified with real hardware that the patterns match the status LEDs on the board
+
+	// Set when clearing waveram memory during initialization:
 	// uint16_t bank = ((~data) >> 6) & 3;
 	// uint16_t offset = (!!((~data) & (1 << 5))) * 8192;
 	// uint16_t verify = !!((~data) & (1 << 4)); // 0 = writing memory, 1 = verifying memory
