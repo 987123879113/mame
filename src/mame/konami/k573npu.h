53fd201b417d84af9669e9e7cb2812d88a39057f
987123879113 <63495610+987123879113@users.noreply.github.com>
2024-02-23 00:47:04 +0900

    konami/k573npu: WIP implementation of the System 573's Network PCB unit (still broken)

--- src/mame/konami/k573npu.h
+++ src/mame/konami/k573npu.h
@@ -1,5 +1,5 @@
 // license:BSD-3-Clause
-// copyright-holders:smf
+// copyright-holders:windyfairy
 /*
  * Konami 573 Network PCB Unit
  *
@@ -10,8 +10,12 @@
 #pragma once
 
 #include "cpu/tx3927/tx3927.h"
-
-
+#include "bus/ata/ataintf.h"
+#include "bus/ata/atapicdr.h"
+#include "machine/ds2401.h"
+#include "machine/ins8250.h"
+#include "machine/ram.h"
+#include "machine/timekpr.h"
 
 DECLARE_DEVICE_TYPE(KONAMI_573_NETWORK_PCB_UNIT, k573npu_device)
 
@@ -22,12 +26,23 @@ public:
 
 protected:
 	virtual void device_start() override;
-	virtual void device_add_mconfig(machine_config &config) override;
+	virtual void device_reset() override;
+	virtual void device_add_mconfig(machine_config& config) override;
 
-	virtual const tiny_rom_entry *device_rom_region() const override;
+	void amap(address_map& map);
+
+	virtual const tiny_rom_entry* device_rom_region() const override;
 
 private:
+	required_device<ds2401_device> digital_id;
 	required_device<tx3927_device> m_maincpu;
+	required_device<ram_device> m_ram;
+
+	uint16_t fpgasoft_read(offs_t offset, uint16_t mem_mask);
+	void fpgasoft_write(offs_t offset, uint16_t data, uint16_t mem_mask);
+
+	uint16_t fpga_read(offs_t offset, uint16_t mem_mask);
+	void fpga_write(offs_t offset, uint16_t data, uint16_t mem_mask);
 };
 
 #endif // MAME_KONAMI_K573NPU_H
